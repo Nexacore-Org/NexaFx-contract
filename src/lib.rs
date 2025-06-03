18c7b9a923ef3a8e1cb8a2a8bba68f267e69dbb5
Lynndabel <okolo.ebelelynda@gmail.com>
2025-06-03 13:25:01 +0100

    feat: escrow contract for disputed transactions with time lock

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 pub mod multisig;
 pub mod escrow;
 pub mod token;
+pub mod rate_lock;
+// Temporarily commenting out modules with errors to focus on testing the rate_lock implementation
+// pub mod swap;
+// pub mod utils;
 
 pub use multisig::MultiSigContract;
 pub use escrow::EscrowContract;
-pub use token::TokenContract; 
+pub use token::TokenContract;
+pub use rate_lock::RateLockContract;
